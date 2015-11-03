f98cfa515207545d3a0c888999f3b6740ab03966
Kumar Ayush <cheekujodhpur@gmail.com>
2015-11-03 05:25:03 -0500

    make grid with deduced dipole and compare

--- main.f95
+++ main.f95
@@ -4,11 +4,20 @@ use SHTOOLS
 
 real :: x,y,z
 real, parameter :: flux_thresh=30e-3
-integer :: lmax
-real*8 :: cilm(2,121,121), grid(180,360)
+integer :: lmax,nlat,nlong
+real*8 :: cilm(2,121,121), grid(181,361), cilm2(2,121,121), grid2(181,361)
 
-do i = 1,180
-    do j = 1,360
+!empty the shit out of these
+do i = 1,2
+    do j = 1,121
+        do k = 1,121
+            cilm2(i,j,k) = 0
+        end do
+    end do
+end do
+
+do i = 1,181
+    do j = 1,361
         grid(i,j) = 0
     end do
 end do
@@ -24,6 +33,7 @@ do while(1 == 1)
 end do
 
 200 CONTINUE
+    !print *,grid
     print *,'Read and process done!'
     close(10)
     call SHExpandDH(grid,180,cilm,lmax,sampling=2)
@@ -34,6 +44,15 @@ end do
             print *,cilm(1,l,k),cilm(2,l,k)
         end do
     end do
+    !put a dipole
+    cilm2(1,1,1)= 0
+    cilm2(1,2,1)= cilm(1,2,1)
+    cilm2(1,2,2)= cilm(1,2,2)
+    cilm2(2,2,2)= cilm(2,2,2)
+    lmax = 89
+    call MakeGrid2D(grid2, cilm2, lmax, 1.0d0,nlat,nlong)
+    print *,'maximum at ',maxloc(grid2)
+    print *,'minimum at ',minloc(grid2)
 
 
 end program main
